5e86a1b49dd5a8145b533178dcde5c321496b564
zero <carlos.cupe@utec.edu.pe>
2019-03-28 21:07:44 -0500

    Primer Intento

--- main.cpp
+++ main.cpp
@@ -2,6 +2,54 @@
 
 using namespace std;
 
+void checkMax(int x, int y, int &min, int &max) {
+    if (x > y) {
+        min = y;
+        max = x;
+    }
+    else {
+        min = x;
+        max = y;
+    }
+}
+
+void problemRule(int &n) {
+    if (n % 2 == 1) {
+        n = n * 3 + 1;
+    }
+    else {
+        n = n >> 1;
+    }
+}
+
+int testCase(int n) {
+    int max_cycles = 1;
+    
+    while(n != 1) {
+        problemRule(n);
+        max_cycles++;
+    }
+
+    return max_cycles;
+}
+
+
 int main(int argc, char *argv[]) {
+    int x, y, min, max;
+    int max_cycles = 1;
+    int temp_max_cycles = 0;
+    cin >> x >> y;
+
+    checkMax(x, y, min, max);
+    
+    for (int i = min; i <= max; i++) {
+        temp_max_cycles = testCase(i);
+        
+        if (temp_max_cycles > max_cycles)
+            max_cycles = temp_max_cycles;
+    }
+
+    cout << x << " " << y << " " << max_cycles <<endl;
+
     return 0;
 }
